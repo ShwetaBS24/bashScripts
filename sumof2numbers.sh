671f1bc14a0d02628a5cec245af3c3abafc689db
ShwetaBS24 <164662664+ShwetaBS24@users.noreply.github.com>
2024-04-06 22:04:39 +0530

    Update sumof2numbers.sh

--- sumof2numbers.sh
+++ sumof2numbers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sum=`expr 10 + 20`
-echo "The sum is $sum"
+addition=`expr 10 + 20`
+substraction=`expr 20 - 5`
+multiplication=`4 \* 10`
+division=`expr 4 / 2`
+echo "The sum is $addition"
+echo "The sum is $substraction"
+echo "The sum is $multiplication"
+echo "The sum is $division"
