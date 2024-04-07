84e07f8cba5ce83084faae77dd631d1b33b95faa
ShwetaBS24 <164662664+ShwetaBS24@users.noreply.github.com>
2024-04-07 17:52:01 +0530

    Update searchfile.sh

--- searchfile.sh
+++ searchfile.sh
@@ -1,5 +1,6 @@
 #This script is to find the sh files
 #This script is to find the all sh files
+#This script is to find the all sh files
 #!/bin/bash
 file_starts_with_name_file=$(ls | grep -i ".sh$")
 echo "$file_starts_with_name_file"
